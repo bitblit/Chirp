d2cfc5d7e4dafe3bf95193450103594b3d8ffbd9
Chris Weiss <bitblit@gmail.com>
2014-10-22 19:42:13 -0700

    work

--- static-site/src/main/webapp/js/chirp.js
+++ static-site/src/main/webapp/js/chirp.js
@@ -1,5 +1,16 @@
 // Library for chirp
 
 $(document).ready(function () {
-    console.log("Document ready placeholder");
+    $.ajax({
+        type: "GET",
+        url: "/api/v1/info/server",
+        contentType: "application/json; charset=utf-8",
+        success: function (res, statusText, xhr, $form) {
+            $("serverTime").html(res.serverTime);
+        },
+        error: function (res, statusText, xhr, $form) {
+            $("serverTime").html("<h1>Error trying to lookup time: "+statusText+"</h1>");
+        },
+        dataType: 'json'
+    });
 });
