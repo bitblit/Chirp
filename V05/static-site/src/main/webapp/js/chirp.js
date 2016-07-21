b1ed7984c3f46a4443a3252ddfa9d46832a6123c
Chris Weiss <chris.weiss@timeinc.com>
2016-07-21 00:38:45 -0700

    Fixes

--- V05/static-site/src/main/webapp/js/chirp.js
+++ V05/static-site/src/main/webapp/js/chirp.js
@@ -1,14 +1,59 @@
 // Chirp things
 
+var api = apigClientFactory.newClient();
+var pingInterval = 500;
+console.log("Api interface created");
+
 $(document).ready(function () {
     console.log("Chirp page loaded");
-    var api = apigClientFactory.newClient();
-    console.log("Api interface created : "+api);
 
-    api.serverStatusGet({'error':''}, {}, {}).then(function (data) {
-        $("#output").html(JSON.stringify(data));
-    }, function (err) {
-        alert("There was an error!\n\n" + JSON.stringify(err));
+    // Start the update pump
+    updateChirps();
+    $("#sendChirpBtn").click(postChirp);
+
+    $("#chirpText").keyup(function(ev) {
+        // 13 is ENTER
+        if (ev.which === 13) {
+            postChirp();
+        }
     });
 
 });
+
+function postChirp()
+{
+    console.log("Post chirp");
+    var text = $("#chirpText").val();
+    if (!!text)
+    {
+        $("#chirpText").val("");
+        api.chirpPost({},{'chirp-text':text},{}).then(function(data){
+            console.log("Rval : "+JSON.stringify(data));
+        },
+        function(err){
+           console.log("Error : "+err);
+        });
+    }
+}
+
+// Setup as a standalone function so we can do a timeout poll
+function updateChirps()
+{
+    api.chirpGet({},{},{}).then(function(data){
+            var allChirps = data.data.data.reverse(); // to get in time order
+
+            var newHtml = "";
+            $.each(allChirps, function (idx, chirp){
+                newHtml+='<li class="list-group-item">';
+                newHtml+='At '+new Date(chirp.timestamp*1000)+', '+chirp.userId+' said : '+chirp.chirp_text;
+                newHtml+='</li>';
+            });
+            $("#chirpList").html(newHtml);
+            setTimeout(updateChirps, pingInterval);
+        },
+        function(err){
+            console.log("Error loading chirps! "+err);
+            setTimeout(updateChirps, pingInterval);
+        })
+
+}
